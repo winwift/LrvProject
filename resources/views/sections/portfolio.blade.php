f1524e763ff5dce1b5110f9f5ab3dad726553e37
winwift <zavadilluke@gmail.com>
2022-10-20 22:32:27 +0100

    Web update

--- resources/views/sections/portfolio.blade.php
+++ resources/views/sections/portfolio.blade.php
@@ -1,58 +1,52 @@
         <!-- Portfolio-->
         <section id="portfolio">
-            <div class="container-fluid p-0">
-                <div class="row g-0">
-                    <div class="col-lg-4 col-sm-6">
+            <div class="container-fluid p-0" >
+                <div class="row g-0" style="justify-content: center">
+                    <div class="col-lg-4 col-sm-6" >
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="Project Name">
-                            <img class="img-fluid" src="{{URL::to('/')}}/images/plumbing.png" alt="..." />
+                            <img class="img-fluid pf-img" src="{{URL::to('/')}}/images/instalation.png"alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Bedroom </div>
                             </div>
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="Project Name">
-                            <img class="img-fluid" src="{{URL::to('/')}}/images/bathroom.png" alt="..." />
+                            <img class="img-fluid pf-img" src="{{URL::to('/')}}/images/bathroom.png" alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Bathroom</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
-                            <img class="img-fluid" src="{{URL::to('/')}}/images/kitchen.png" alt="..." />
+                            <img class="img-fluid pf-img"  src="{{URL::to('/')}}/images/kitchen.png" alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Kitchen</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
-                            <img class="img-fluid" src="{{URL::to('/')}}/images/painting.png" alt="..." />
+                            <img class="img-fluid pf-img" src="{{URL::to('/')}}/images/painting.png" alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Decoration</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
+                            <img class="img-fluid pf-img"  src="{{URL::to('/')}}/images/plumbing.png" alt="..." />
                             <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Plumbing</div>
                             </div>
                         </a>
                     </div>
                     <div class="col-lg-4 col-sm-6">
                         <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
+                            <img class="img-fluid pf-img" src="{{URL::to('/')}}/images/renovation.png" alt="..." />
                             <div class="portfolio-box-caption p-3">
-                                <div class="project-category text-white-50">Category</div>
-                                <div class="project-name">Project Name</div>
+                                <div class="project-name">Demolition</div>
                             </div>
                         </a>
                     </div>
