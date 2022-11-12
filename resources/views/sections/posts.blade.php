cbd30b9476c0c81e405b09c85f4608bf1f0932f4
Lukas Zavadil <zavadilluke@gmail.com>
2022-11-12 00:59:32 +0000

    Card grid update

--- resources/views/sections/posts.blade.php
+++ resources/views/sections/posts.blade.php
@@ -2,43 +2,41 @@
 
 @unless(count($posts) == 0)
 <!--Posts-->
-<section id="jobs" class="container-fluid p-0">
-    <div class="row g-0" id="portfolio_id">
+<section class="container-fluid p-0">
+    <div class="row" style="align-items: end"  >
 @foreach($posts as $post)
-        <div class="col-lg-4 col-sm-6" >
-                        <div class="card mb-4">
-                            
-                            <img class="card-img-top" src="{{$post->getFirstMediaUrl('posts')}}" alt="..." />
-                            
-                            <div class="card-body">
+        <div class="col-4 " >
+                <div class="card ">
+                    <img class="card-img-top" src="{{$post->getFirstMediaUrl('posts')}}" alt="..." />
+                    <div class="card-body">
                                 
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div>
-                                    <a class="card-title text-decoration-none"  href="/posts/{{$post['id']}}" >{{$post['title']}}</a>
-                                    </div>
-                                    <div>
-                                    <small class="text-muted">Category:</small>
-                                    <a href="/posts/{{$post['id']}}" class="text-decoration-none card-subtitle">{{$post->category->name}}</a>
-                                    </div>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <a class="card-title text-decoration-none"  href="/posts/{{$post['id']}}" >{{$post['title']}}</a>
                                 </div>
+                                <div>
+                                <small class="text-muted">Category:</small>
+                                <a href="/posts/{{$post['id']}}" class="text-decoration-none card-subtitle">{{$post->category->name}}</a>
+                                </div>
+                            </div>
 
-                                <p class="card-text pt-3">{{ Str::limit($post->description, 120)}}</p>
-                                <div class="d-flex justify-content-between align-items-center">
-
-                                    <div>
-                                    @foreach($post->tags as $tag)
-                                        <span class="badge bg-primary">{{$tag->name}}</span>
-                                    @endforeach
-                                    </div>
+                            <p class="card-text pt-3">{{ Str::limit($post->description, 120)}}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div>
+                                  @foreach($post->tags as $tag)
+                                    <span class="badge bg-primary">{{$tag->name}}</span>
+                                @endforeach
+                                </div>
 
 
                                 <small class="text-muted">{{ $post->value('created_at') }}</small>   
-                                </div>
+                            </div>
                                 
                             
                             </div>
                             
-                    </div> 
+                </div>
+        </div>
 @endforeach  
     </div>
 <div class="d-flex justify-content-center">
